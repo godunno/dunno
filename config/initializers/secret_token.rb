dfaaa85cdf0b70831b76534e16ba10ca1fca2d24
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-02-05 14:50:33 -0200

    Replace secret token with environment variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Dunno::Application.config.secret_key_base = '2353ce337d5545f884614eacffd915414164d71fcd3914dfea5a0c799962c1e4b26889e0f1f6c7cae5ae4349bdf7fe680e6ac7d1a6e568607b0d04b91c2d7680'
+Dunno::Application.config.secret_key_base = ENV['SECRET_TOKEN']
