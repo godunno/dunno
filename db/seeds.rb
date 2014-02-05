d53c1c1ff17048897667d2d42e0dbef89ed04feb
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-02-05 21:08:15 -0200

    Add seeds to test.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Rails.env.development? || Rails.env.staging?
+  if Organization.count.zero?
+    org = Organization.new(name: "Dunno Test")
+    org.save!
+
+    if Event.count.zero?
+      Event.new(title: "1st class",start_at: 1.days.from_now, organization: org).save!
+      Event.new(title: "2nd class",start_at: 3.days.from_now, organization: org).save!
+      Event.new(title: "3th class",start_at: 5.days.from_now, organization: org).save!
+      Event.new(title: "4th class",start_at: 7.days.from_now, organization: org).save!
+      Event.new(title: "5th class",start_at: 9.days.from_now, organization: org).save!
+    end
+  end
+end
