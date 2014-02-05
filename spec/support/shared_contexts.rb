952dd5a399abd07b1e3589a8195224e90e3c1ca8
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-02-05 17:37:01 -0200

    Adding shared contexts and helper to support request specs.

---
+++ spec/support/shared_contexts.rb
@@ -0,0 +1,15 @@
+shared_examples_for "API authentication required" do
+
+  context "when not authenticated" do
+
+    pending "Authentication not implemented yet."
+  end
+end
+
+
+shared_examples_for "request invalid content type XML" do
+
+  it do
+    expect { do_action }.to raise_error(ActionController::UnknownFormat)
+  end
+end
