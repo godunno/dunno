838b35e07d72c8f7dea958ab472c1dfc1eab6981
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-02-05 16:24:58 -0200

    Adding validations to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ActiveRecord::Base
+  belongs_to :organization
+
+  validates :title, :start_at, presence: true
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to(:organization) }
+  end
+  describe "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:start_at) }
+  end
 end
