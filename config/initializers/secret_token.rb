c389252221fff0fa83c12f30f37d2386b94597e2
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-02-05 14:49:24 -0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dunno::Application.config.secret_key_base = '2353ce337d5545f884614eacffd915414164d71fcd3914dfea5a0c799962c1e4b26889e0f1f6c7cae5ae4349bdf7fe680e6ac7d1a6e568607b0d04b91c2d7680'
