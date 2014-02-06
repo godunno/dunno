21757c4e2f946f9d2ba964972664d7c5639a9086
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-02-06 16:32:31 -0200

    Generating uuid to organization after create it

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,4 +2,12 @@ class Organization < ActiveRecord::Base
   has_many :events
 
   validates :name, presence: true
+
+  after_create :set_uuid
+
+
+  private
+    def set_uuid
+      UuidGenerator.new(self).generate!
+    end
 end
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -9,4 +9,38 @@ describe Organization do
   describe "validations" do
     it { should validate_presence_of(:name) }
   end
+
+  describe "callbacks" do
+
+    describe "after create" do
+
+      let!(:uuid) { "ead0077a-842a-4d35-b164-7cf25d610d4d" }
+      let(:organization) { build(:organization, name: "New school of development")  }
+
+      context "new organization" do
+        before(:each) do
+          SecureRandom.stub(:uuid).and_return(uuid)
+        end
+
+        it "generates an uuid" do
+          expect do
+            organization.save!
+          end.to change{organization.uuid}.from(nil).to(uuid)
+        end
+      end
+
+      context "existent organization" do
+        before(:each) do
+          SecureRandom.stub(:uuid).and_return(uuid)
+          organization.save!
+        end
+
+        it "does not generates an uuid" do
+          expect do
+            organization.save!
+          end.to_not change{organization.uuid}
+        end
+      end
+    end
+  end
 end
