8ce84488f8af56944667d1b98212d78272b74a2c
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-02-06 11:26:09 -0200

    Adding relation between event and timeline.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :organization
+  has_many :timelines
 
   validates :title, :start_at, presence: true
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Event do
   describe "associations" do
+    it { should have_many(:timelines) }
     it { should belong_to(:organization) }
   end
   describe "validations" do
