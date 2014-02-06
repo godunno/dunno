73dddb3e80d633fdf3f5f9ff7e6f7c94d736fb3b
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-02-06 11:22:02 -0200

    Adding validations and associations to timeline model. Closes #18.

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -1,2 +1,6 @@
 class Timeline < ActiveRecord::Base
+  belongs_to :interaction, polymorphic: true
+  belongs_to :event
+
+  validates :start_at, presence: true
 end
--- spec/models/timeline_spec.rb
+++ spec/models/timeline_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Timeline do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "association" do
+    it { should belong_to(:interaction) }
+    it { should belong_to(:event) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:start_at) }
+  end
 end
