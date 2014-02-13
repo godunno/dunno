6ff166310ffa40498534bf43caf7117aba36492f
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-02-13 11:22:48 -0200

    Adding duration to event serializer.

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,6 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :organization_id, :id, :title, :uuid, :channel, :student_message_event, :up_down_vote_message_event, :receive_poll_event, :receive_rating_event
+  attributes :organization_id, :id, :title, :uuid, :duration, :channel, :student_message_event,
+             :up_down_vote_message_event, :receive_poll_event, :receive_rating_event
 
   has_one :timeline
 end
