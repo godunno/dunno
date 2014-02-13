d00df6d3d8d08105280bc28a1dd1d4226b9f5625
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-02-13 12:40:22 -0200

    Fix seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,15 @@ if Rails.env.development? || Rails.env.staging?
     org = Organization.new(name: "Dunno Test")
     org.save!
 
+    teacher = Teacher.new(name: "Prof. Example", email: "prof@dunno.vc", password: "#dunnovc", password_confirmation: "#dunnovc")
+    teacher.organizations << org
+
     if Event.count.zero?
-      Event.new(title: "1st class",start_at: 1.days.from_now, organization: org, status: "available").save!
-      Event.new(title: "2nd class",start_at: 3.days.from_now, organization: org, status: "available").save!
-      Event.new(title: "3th class",start_at: 5.days.from_now, organization: org, status: "available").save!
-      Event.new(title: "4th class",start_at: 7.days.from_now, organization: org, status: "available").save!
-      Event.new(title: "5th class",start_at: 9.days.from_now, organization: org, status: "available").save!
+      Event.new(title: "1st class",start_at: 1.days.from_now, teacher: teacher, organization: org, status: "available").save!
+      Event.new(title: "2nd class",start_at: 3.days.from_now, teacher: teacher, organization: org, status: "available").save!
+      Event.new(title: "3th class",start_at: 5.days.from_now, teacher: teacher, organization: org, status: "available").save!
+      Event.new(title: "4th class",start_at: 7.days.from_now, teacher: teacher, organization: org, status: "available").save!
+      Event.new(title: "5th class",start_at: 9.days.from_now, teacher: teacher, organization: org, status: "available").save!
     end
   end
 end
