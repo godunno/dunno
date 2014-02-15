90c738a0183a11b8f0240a5e9f82c427a27928de
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-02-15 19:14:27 -0200

    Adding authentication token ensure generation to Student model.

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,4 +6,19 @@ class Student < ActiveRecord::Base
 
   belongs_to :organization
 
+  before_save :ensure_authentication_token
+
+  private
+    def ensure_authentication_token
+      if authentication_token.blank?
+        self.authentication_token = generate_authentication_token
+      end
+    end
+
+    def generate_authentication_token
+      loop do
+        token = Devise.friendly_token
+        break token unless Student.where(authentication_token: token).first
+      end
+    end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -4,4 +4,24 @@ describe Student do
   describe "association" do
     it { should belong_to(:organization) }
   end
+
+  describe "callbacks" do
+    describe "before save" do
+      describe "ensures authentication token" do
+        context "when student does not have a token" do
+          let(:student) { build(:student) }
+
+          it do
+            student.save
+            expect(student.authentication_token).to_not be_nil
+          end
+        end
+        context "when student already have a token" do
+          let!(:student) { create(:student) }
+
+          it { expect{ student.save }.to_not change{ student.reload.authentication_token } }
+        end
+      end
+    end
+  end
 end
