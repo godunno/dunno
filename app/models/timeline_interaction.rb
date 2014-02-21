26e2de6919b9f8515c356d34c1e74169a0b4265b
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-02-21 15:26:47 -0300

    Adding messages scope to timeline interaction.

--- app/models/timeline_interaction.rb
+++ app/models/timeline_interaction.rb
@@ -1,4 +1,6 @@
 class TimelineInteraction < ActiveRecord::Base
   belongs_to :timeline
   belongs_to :interaction, polymorphic: true
+
+  scope :messages, -> { where(interaction_type: "TimelineUserMessage").order(:created_at) }
 end
