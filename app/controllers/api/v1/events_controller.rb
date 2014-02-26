acfe01cd651259604cc745e2f47241a26c311922
Fagner Moura <fagnermoura@gmail.com>
2014-02-26 17:40:41 -0300

    remove root node on attend event ws

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::EventsController < Api::V1::ApplicationController
 
   def attend
     if event.opened?
-      respond_with event
+      respond_with event, root: false
     else
       render nothing: true, status: 403
     end
