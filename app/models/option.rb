e959529c675547141d83ca15b398f8d3e89f92a1
Marcos Rodrigues <mrodrigues.uff@gmail.com>
2014-03-11 17:44:16 -0300

    Set uuid on save for options

--- app/models/option.rb
+++ app/models/option.rb
@@ -2,4 +2,11 @@ class Option < ActiveRecord::Base
   belongs_to :poll
 
   validates :content, presence: true
+
+  after_create :set_uuid
+
+  private
+    def set_uuid
+      UuidGenerator.new(self).generate!
+    end
 end
--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -1,11 +1,34 @@
 require 'spec_helper'
 
 describe Option do
+
+  let(:option) { build :option }
+
   describe "associations" do
     it { should belong_to(:poll) }
   end
 
   describe "validations" do
     it { should validate_presence_of(:content) }
+  end
+
+  describe "callbacks" do
+
+    describe "after create" do
+
+      let!(:uuid) { "ead0077a-842a-4d35-b164-7cf25d610d4d" }
+
+      context "new option" do
+        before(:each) do
+          SecureRandom.stub(:uuid).and_return(uuid)
+        end
+
+        it "saves a new uuid" do
+          expect do
+            option.save!
+          end.to change{option.uuid}.from(nil).to(uuid)
+        end
+      end
+    end
   end
 end
