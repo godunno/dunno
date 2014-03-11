de78d1ad3e7e4ff88c5db3edf776386ef7c42a18
Marcos Rodrigues <mrodrigues.uff@gmail.com>
2014-03-11 16:46:17 -0300

    Button to release poll

--- app/controllers/dashboard/polls_controller.rb
+++ app/controllers/dashboard/polls_controller.rb
@@ -2,7 +2,7 @@ class Dashboard::PollsController < Dashboard::ApplicationController
   def release
     poll.update(status: "released")
     EventPusher.new(poll.event).release_poll(poll)
-    render nothing: true, status: 200
+    redirect_to dashboard_organization_events_path(poll.event.organization.uuid)
   end
 
   private
