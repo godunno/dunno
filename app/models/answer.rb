0727816d0458cc83b4266a82903b3264e9b292f8
Marcos Rodrigues <mrodrigues.uff@gmail.com>
2014-03-12 16:19:21 -0300

    Forbid two answers with the same student and poll

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,13 @@
 class Answer < ActiveRecord::Base
   belongs_to :student
   belongs_to :option
+
+  validate :validate_uniqueness_per_student_per_poll
+
+  private
+    def validate_uniqueness_per_student_per_poll
+      if Answer.joins(option: :poll).where(student: student, options: { poll_id: option.poll }).any?
+        errors.add :student, :taken
+      end
+    end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,4 +5,17 @@ describe Answer do
     it { should belong_to(:option) }
     it { should belong_to(:student) }
   end
+
+  describe "validations" do
+
+    before do
+      poll = create(:poll)
+      student = create(:student)
+      Answer.create!(option: create(:option, poll: poll), student: student)
+      @duplicated_answer = Answer.new(option: create(:option, poll: poll), student: student)
+    end
+
+    it { expect(@duplicated_answer).to_not be_valid }
+    it { expect(@duplicated_answer).to have(1).error_on(:student) }
+  end
 end
