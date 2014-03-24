d7a9fbf95e15355eede6abb3132225ed4f2a1640
Fagner Moura <fagnermoura@gmail.com>
2014-03-24 16:57:43 -0300

    add classroom to course serializer

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,5 +1,6 @@
 class CourseSerializer < ActiveModel::Serializer
   attributes :id, :name, :uuid, :start_date, :end_date, :start_time,
-    :end_time, :weekdays
+    :end_time, :weekdays, :classroom
+
   has_one :teacher
 end
