fa434d72fb24a6c95a32293827fc332f08a5b8d7
mrodrigues <mrodrigues.uff@gmail.com>
2014-04-02 01:09:04 -0300

    Removing timeline serializer

--- app/serializers/timeline_serializer.rb
+++
@@ -1,7 +0,0 @@
-class TimelineSerializer < ActiveModel::Serializer
-  attributes :id, :start_at, :created_at, :updated_at, :messages
-
-  def messages
-    object.timeline_interactions.messages.map { |ti| ti.interaction }
-  end
-end
