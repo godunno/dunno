6671c3f973ce98a4df96039d9925fa43727b38ce
mrodrigues <mrodrigues.uff@gmail.com>
2014-07-16 16:20:02 -0300

    Fixing documentation

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,8 @@
 Apipie.configure do |config|
   config.app_name                = "Dunno"
-  config.api_base_url            = ""
+  config.api_base_url            = nil
   config.doc_base_url            = "/apipie"
   # were is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/**/*.rb"
+  config.namespaced_resources = true
 end
