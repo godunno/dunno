a54089112a1130609d860446d4e1725f40e94799
mrodrigues <mrodrigues.uff@gmail.com>
2014-08-05 01:12:07 -0300

    Debugging pre-compilation error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ Dunno::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.debug = true
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
