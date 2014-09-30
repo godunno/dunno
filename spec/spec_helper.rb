ff7c1a993e80138590c79f838711f5679eec1cb4
Pedro Nascimento <pnascimento@gmail.com>
2014-09-29 21:56:39 -0300

    Removing GC optimisation, irrelevant on 2.1.0.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -57,48 +57,9 @@ RSpec.configure do |config|
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 
-  config.before(:each) do
-    begin_gc_deferment
-  end
-
   config.before(:each, :type => :request) do
     header 'ACCEPT', 'application/json'
     header 'CONTENT_TYPE', 'application/json'
-  end
-
-  config.after(:each) do
-    scrub_instance_variables
-    reconsider_gc_deferment
-  end
-end
-
-# http://blog.29steps.co.uk/post/24145533872/how-to-optimize-your-rspec-tests
-# https://gist.github.com/cheeyeo/790094
-# Rspec optmization techniques borrowed from 37Signals
-
-def scrub_instance_variables
-  instance_variable_set(:@__memoized, nil)
-end
-
-
-# http://blog.29steps.co.uk/post/24145533872/how-to-optimize-your-rspec-tests
-# https://gist.github.com/cheeyeo/790118
-# Optimizing the garbage collector in Rspec
-DEFERRED_GC_THRESHOLD = (ENV['DEFER_GC'] || 1.0).to_f
-@@last_gc_run = Time.now
-
-def begin_gc_deferment
-  GC.disable if DEFERRED_GC_THRESHOLD > 0
-end
-
-def reconsider_gc_deferment
-  last_gc_run = self.class.class_variable_get(:@@last_gc_run)
-  if DEFERRED_GC_THRESHOLD > 0 && Time.now - last_gc_run >= DEFERRED_GC_THRESHOLD
-    GC.enable
-    GC.start
-    GC.disable
-
-    last_gc_run = Time.now
   end
 end
 
