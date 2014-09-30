35ef95bcddfa270d92c7efff71e1c03a9662e779
Pedro Nascimento <pnascimento@gmail.com>
2014-09-30 00:33:11 -0300

    Removing some clutter from config/application.rb and adding i18n config to remove deprecation.

--- config/application.rb
+++ config/application.rb
@@ -23,24 +23,10 @@ module Dunno
     # add custom validators path
     config.autoload_paths += %W["#{config.root}/app/validators/"]
 
-    # Prevent initializing your application and connect to the database on assets precompile.
     config.assets.initialize_on_precompile = false
     config.assets.precompile += %w( modernizr/modernizr )
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Brasilia'
-
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-    #config.i18n.enforce_available_locales = true
+    config.i18n.enforce_available_locales = true
     config.i18n.default_locale = :"pt-BR"
     config.i18n.locale = :"pt-BR"
 
