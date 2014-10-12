a640069ba34ffbe75832ee33cf07c712286c6179
Pedro Nascimento <pnascimento@gmail.com>
2014-10-12 05:05:19 -0300

    More rubocop fixes.

--- spec/support/acceptance_helpers.rb
+++ spec/support/acceptance_helpers.rb
@@ -3,6 +3,7 @@ module AcceptanceHelpers
   def saop
     save_and_open_page
   end
+
   def handle_js_confirm(message = nil, accept = true)
     page.execute_script("window.original_confirm_function = window.confirm")
     page.execute_script("window.confirm = function(msg) { $.cookie('confirm_message', msg); return #{!!accept}; }")
@@ -11,10 +12,12 @@ module AcceptanceHelpers
   ensure
     page.evaluate_script "window.confirm = window.original_confirm_function"
   end
+
   def fill_in_autocomplete(capybara_selector, jquery_selector, value)
     fill_in capybara_selector, :with => value
     page.execute_script %Q{$('#{jquery_selector}').keydown()}
   end
+
   def choose_autocomplete(text)
     find('ul.ui-autocomplete').should have_content(text)
     page.execute_script("$('.ui-menu-item:contains(\"#{text}\")').find('a').trigger('mouseenter').click()")
@@ -22,5 +25,5 @@ module AcceptanceHelpers
 end
 
 RSpec.configure do |config|
-  config.include AcceptanceHelpers, :type => :request
+  config.include AcceptanceHelpers, type: :request
 end
