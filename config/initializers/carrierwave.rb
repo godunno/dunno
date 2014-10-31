5a0798f73326031520244f52049b92f1faea5ba3
mrodrigues <mrodrigues.uff@gmail.com>
2014-10-31 19:05:04 -0200

    Configuring S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,20 @@
 require 'carrierwave/orm/activerecord'
 
-if %w(staging production).include? Rails.env
-  #CarrierWave.configure do |config|
-  #  config.fog_credentials = {
-  #    :provider               => 'AWS',                         # required
-  #    :aws_access_key_id      => ENV['aws_access_key_id'],      # required
-  #    :aws_secret_access_key  => ENV['aws_secret_access_key'],  # required
-  #    #:region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-  #    #:host                   => 's3.example.com',             # optional, defaults to nil
-  #    #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
-  #  }
-  #  config.fog_directory  = ENV['aws_bucket_name']                  # required
-  #  config.fog_public     = false                                   # optional, defaults to true
-  #  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-  #end
+CarrierWave.configure do |config|
+  if Rails.env.test?
+    config.storage :file
+  else
+    config.storage :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',                         # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],      # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],  # required
+      #:region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+      #:host                   => 's3.example.com',             # optional, defaults to nil
+      #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+    }
+    config.fog_directory  = ENV['AWS_BUCKET_NAME']                  # required
+    config.fog_public     = false                                   # optional, defaults to true
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  end
 end
