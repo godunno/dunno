0020ca5f026d92dff20e8b2255bbece21ce886a4
Pedro Nascimento <pnascimento@gmail.com>
2014-11-06 18:33:05 -0200

    Adding some helpers for logging in with user token and email/password.

--- app/controllers/api/v2/application_controller.rb
+++ app/controllers/api/v2/application_controller.rb
@@ -1,2 +1,28 @@
-class API::V2::ApplicationController < ActionController::Base
+class Api::V2::ApplicationController < ActionController::Base
+  helper_method :current_user
+  respond_to :json
+
+  def current_user
+    @current_user ||= User.find(@_current_user_id) if @_current_user_id
+  rescue ActiveRecord::RecordNotFound
+    @_current_user_id = nil
+    nil
+  end
+
+  def authenticate_user!
+    authenticate_or_request_with_http_token do |token, options|
+      if api_key = ApiKey.find_by(token: token)
+        @_current_user_id = api_key.user_id
+      end
+    end
+  end
+
+  def authenticate_user_with_credentials!
+    authenticate_or_request_with_http_basic do |email, password|
+      user = User.find_by(email: email)
+      if user && user.valid_password?(password)
+        @_current_user_id = user.id
+      end
+    end
+  end
 end
