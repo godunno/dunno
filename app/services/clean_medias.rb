72223f3b310116ea1529eff2208b2de2540dcc53
mrodrigues <mrodrigues.uff@gmail.com>
2014-11-06 18:04:25 -0200

    Should clean only medias at least 1 month old

--- app/services/clean_medias.rb
+++ app/services/clean_medias.rb
@@ -1,5 +1,5 @@
 class CleanMedias
   def self.clean!
-    Media.where(topic: nil).find_each(&:destroy!)
+    Media.where(topic: nil).where('created_at <= ?', 1.month.ago).find_each(&:destroy!)
   end
 end
