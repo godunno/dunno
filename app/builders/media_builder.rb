94b8768e9ce1c3642cb495def8c7da69c3f9a76f
mrodrigues <mrodrigues.uff@gmail.com>
2014-11-12 20:37:09 -0200

    Updating Medias' teachers

--- app/builders/media_builder.rb
+++ app/builders/media_builder.rb
@@ -2,7 +2,6 @@ class MediaBuilder < BaseBuilder
   def build(json = Jbuilder.new, options = {})
     json.(media, :uuid, :title, :description, :category, :preview, :type)
     json.released_at(format_time(media.released_at))
-
     json.url(media.url || media.file.url)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112175341) do
+ActiveRecord::Schema.define(version: 20141112223344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
