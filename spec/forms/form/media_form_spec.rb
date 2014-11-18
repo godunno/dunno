b387499e11387260456b3dfafd1e5c756055d1ea
mrodrigues <mrodrigues.uff@gmail.com>
2014-11-18 17:11:10 -0200

    Improving tests readability

--- spec/forms/form/media_form_spec.rb
+++ spec/forms/form/media_form_spec.rb
@@ -9,15 +9,11 @@ describe Form::MediaForm do
 
     it "should validate URL's format" do
       ["http://www.example.com", "http://example.com", "http://www.example.com/path/", "https://www.example.com"].each do |url|
-        media_form.url = url
-        expect(media_form).to be_valid
-        expect(media_form.errors[:url].count).to eq(0)
+        expect(media_form).to allow_value(url).for(:url)
       end
 
       ["ftp://path.com/file.doc", "www.example.com", "example.com", "example"].each do |url|
-        media_form.url = url
-        expect(media_form).not_to be_valid
-        expect(media_form.errors[:url].count).to eq(1)
+        expect(media_form).not_to allow_value(url).for(:url)
       end
     end
 
