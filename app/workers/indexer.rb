00708849cc25e72268aa98f78fc4fd50a45f418c
mrodrigues <mrodrigues.uff@gmail.com>
2014-11-26 14:17:09 -0200

    Extracting ElasticSearch client to instance method

--- app/workers/indexer.rb
+++ app/workers/indexer.rb
@@ -4,8 +4,9 @@ class Indexer
   include Sidekiq::Worker
   sidekiq_options queue: 'elasticsearch', retry: false
 
-  LOGGER = Sidekiq.logger.level == Logger::DEBUG ? Sidekiq.logger : nil
-  CLIENT = Elasticsearch::Client.new logger: LOGGER
+  def client
+    @client ||= Elasticsearch::Client.new logger: logger
+  end
 
   def perform(operation, record_id)
     logger.debug [operation, "ID: #{record_id}"]
@@ -13,9 +14,9 @@ class Indexer
     case operation.to_s
     when "index"
       record = Media.find(record_id)
-      CLIENT.index index: 'medias', type: 'media', id: record.id, body: record.as_indexed_json
+      client.index index: 'medias', type: 'media', id: record.id, body: record.as_indexed_json
     when "delete"
-      CLIENT.delete index: 'medias', type: 'media', id: record_id
+      client.delete index: 'medias', type: 'media', id: record_id
     else fail ArgumentError, "Unknown operation '#{operation}'"
     end
   end
