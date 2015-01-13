5924c53401704623af254a57a89cac5976e1508e
mrodrigues <mrodrigues.uff@gmail.com>
2015-01-13 01:22:00 -0200

    Configuring beta for email sending

--- config/environments/beta.rb
+++ config/environments/beta.rb
@@ -80,4 +80,16 @@ Dunno::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.roadie.url_options = { host: "beta.dunnoapp.com", scheme: "http" }
+
+  ActionMailer::Base.smtp_settings = {
+    port:           '587',
+    address:        'smtp.mandrillapp.com',
+    user_name:      ENV['MANDRILL_USERNAME'],
+    password:       ENV['MANDRILL_APIKEY'],
+    domain:         'heroku.com',
+    authentication: :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
