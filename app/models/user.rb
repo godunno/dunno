fa04541d67e769bdf2875f84465cb68e038b4e72
mrodrigues <mrodrigues.uff@gmail.com>
2015-01-15 19:05:38 -0200

    Sending emails in background with Sidekiq

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :async
 
   belongs_to :profile, polymorphic: true
   has_many :api_keys
