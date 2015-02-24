2378e7e4c041116a9ff5628af74c7eabf6bbb863
Pedro Nascimento <pnascimento@gmail.com>
2015-02-24 19:54:37 -0300

    Using google_drive to fetch the spreadsheet.
    Hard-coding my user token for now.

--- spec/services/spreadsheet_parser_spec.rb
+++ spec/services/spreadsheet_parser_spec.rb
@@ -1,13 +1,8 @@
 require 'spec_helper'
-require 'webmock/rspec'
 
-describe SpreadsheetParser do
+describe SpreadsheetParser, :vcr do
   describe "parsing the spreadsheet" do
-    let(:spreadsheet) { File.open("spec/fixtures/imports/professores.xlsx") }
-    let(:url) { "http://www.example.com/professores.xlsx" }
-    before do
-      stub_request(:get, url).to_return(body: spreadsheet)
-    end
+    let(:url) { "https://docs.google.com/a/dunnoapp.com/spreadsheets/d/1-UQ3aynn8Khe2vMIsISQNPXUkf9yj-4jen4Ly4eyszs/edit#gid=0" }
 
     let(:values) { ["Zé da Silva", "ze@gmail.com", "21 99999 9999"] }
     it "should return an array with the content" do
@@ -22,12 +17,5 @@ describe SpreadsheetParser do
       SpreadsheetParser.parse("url")
       expect(parser).to have_received(:parse)
     end
-  end
-
-  it "should work on lightweight files" do
-    spreadsheet = File.open("spec/fixtures/imports/lightweight-file.xlsx")
-    url = "http://www.example.com/lightweight-file.xlsx"
-    stub_request(:get, url).to_return(body: spreadsheet)
-    expect { SpreadsheetParser.new(url, header_rows: 1).parse }.not_to raise_error
   end
 end
