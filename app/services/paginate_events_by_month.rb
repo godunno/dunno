3c9a3f9f4c6b3fa7ddf2f348b5a3ac8fa9e16721
Pedro Nascimento <pnascimento@gmail.com>
2015-02-24 16:50:40 -0300

    Try to always show a month with events in Course#show.
    * If the course has events in the given month, the given month is displayed.
    * If it doesn’t, we try to find an event in the future.
    * If we don’t have an event in the future, we look for an event in the past.
    * If we can’t find events at all, show current month.

--- app/services/paginate_events_by_month.rb
+++ app/services/paginate_events_by_month.rb
@@ -1,17 +1,49 @@
 class PaginateEventsByMonth
-  attr_reader :current_month, :events
+  attr_reader :current_month
 
   def initialize(events, day_in_month)
-    day_in_month ||= Time.current
-    @current_month = day_in_month.to_time.in_time_zone.beginning_of_month
-    @events = events.where(start_at: @current_month..@current_month.end_of_month)
+    @all_events = events.order(:start_at)
+
+    if day_in_month.blank? && events_for_month.empty? && all_events.any?
+      @current_month = next_month_with_event || last_month_with_event
+    else
+      day_in_month ||= Time.current
+      @current_month = day_in_month.to_time.in_time_zone.beginning_of_month
+    end
   end
 
   def next_month
-    @current_month + 1.month
+    current_month + 1.month
   end
 
   def previous_month
-    @current_month - 1.month
+    current_month - 1.month
+  end
+
+  def events
+    @events ||= events_for_month(current_month)
+  end
+
+  private
+
+  attr_reader :all_events
+
+  def month_for(day_in_month)
+    day_in_month.to_time.in_time_zone.beginning_of_month
+  end
+
+  def events_for_month(month = Time.current)
+    full_month = month.beginning_of_month..month.end_of_month
+    all_events.where(start_at: full_month)
+  end
+
+  def next_month_with_event
+    event = all_events.where('start_at > ?', Time.current).first
+    event && event.start_at.beginning_of_month
+  end
+
+  def last_month_with_event
+    event = all_events.where('start_at < ?', Time.current).last
+    event && event.start_at.beginning_of_month
   end
 end
