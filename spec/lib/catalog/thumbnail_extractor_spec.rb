fb92346000772c5f8bb6b2b3e1bb9e0c01f8c128
Pedro Nascimento <pnascimento@gmail.com>
2015-03-04 01:45:01 -0300

    Fixing more tests.

--- spec/lib/catalog/thumbnail_extractor_spec.rb
+++ spec/lib/catalog/thumbnail_extractor_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe Catalog::ThumbnailExtractor do
-  def media_double(url: nil, file: nil, original_filename: nil, preview: nil)
-    media = double("media", url: url, file: file, original_filename: original_filename)
+  def media_double(url: nil, file_url: nil, original_filename: nil, preview: nil)
+    media = double("media", url: url, file_url: file_url, original_filename: original_filename)
     allow(media).to receive_message_chain(:preview, :images, :first, :src, to_s: preview)
     media
   end
@@ -24,20 +24,20 @@ describe Catalog::ThumbnailExtractor do
 
   context "from file extension" do
     let(:original_filename) { "document.doc" }
-    let(:file) { "path/to/document.doc" }
-    let(:media) { media_double(file: file, original_filename: original_filename) }
+    let(:file_url) { "path/to/document.doc" }
+    let(:media) { media_double(file_url: file_url, original_filename: original_filename) }
     it { expect(Catalog::ThumbnailExtractor.new(media).extract).to eq("/assets/thumbnails/doc.png") }
   end
 
   context "URL default" do
-    let(:media) { media_double(url: "http://www.example.com") }
+    let(:media) { media_double(url: "http://www.example.com", file_url: nil) }
     it { expect(Catalog::ThumbnailExtractor.new(media).extract).to eq("/assets/thumbnails/url.png") }
   end
 
   context "file default" do
     let(:original_filename) { "invalid_extension.xyz" }
-    let(:file) { "path/to/invalid_extension.xyz" }
-    let(:media) { media_double(file: file, original_filename: original_filename) }
+    let(:file_url) { "path/to/invalid_extension.xyz" }
+    let(:media) { media_double(file_url: file_url, original_filename: original_filename) }
     it { expect(Catalog::ThumbnailExtractor.new(media).extract).to eq("/assets/thumbnails/file.png") }
   end
 end
