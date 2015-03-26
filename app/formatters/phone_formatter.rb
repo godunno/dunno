367eb44687456dd2be71d4d8fcb1f5e0bb1daeed
Pedro Nascimento <pnascimento@gmail.com>
2015-03-26 15:45:38 -0300

    Moving class method to the beginning of the file in PhoneFormatter.

--- app/formatters/phone_formatter.rb
+++ app/formatters/phone_formatter.rb
@@ -2,15 +2,15 @@ class PhoneFormatter
   FORMAT = "+%c %a %n"
   attr_accessor :number
 
+  def self.format(number)
+    new(number).format
+  end
+
   def initialize(number)
     self.number = number
   end
 
   def format
     Phonie::Phone.parse(number, country_code:  "55").format(FORMAT)
-  end
-
-  def self.format(number)
-    new(number).format
   end
 end
