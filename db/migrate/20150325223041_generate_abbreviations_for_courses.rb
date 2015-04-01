f2b988dc8d1c6ac1b224b97b7a2f7a588ae76735
mrodrigues <mrodrigues.uff@gmail.com>
2015-03-31 21:44:21 -0300

    Removing wrong migration

--- db/migrate/20150325223041_generate_abbreviations_for_courses.rb
+++
@@ -1,7 +0,0 @@
-class GenerateAbbreviationsForCourses < ActiveRecord::Migration
-  def change
-    Course.find_each do |course|
-      course.update!(abbreviation: Abbreviate.abbreviate(course.name))
-    end
-  end
-end
