2b3c0ad458d43258b8e829d5818c4b4ba85afcf9
Pedro Nascimento <pnascimento@gmail.com>
2015-04-06 19:30:57 -0300

    Updated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325222842) do
+ActiveRecord::Schema.define(version: 20150325223041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -299,6 +299,7 @@ ActiveRecord::Schema.define(version: 20150325222842) do
     t.integer  "order"
     t.boolean  "done"
     t.integer  "media_id"
+    t.boolean  "personal",    default: false
   end
 
   add_index "topics", ["event_id"], name: "index_topics_on_event_id", using: :btree
