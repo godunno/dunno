5eda7d9985a04e106cd27b884095b1ecca372f1e
Pedro Nascimento <pnascimento@gmail.com>
2015-05-13 14:08:17 -0300

    Adding a TODO for regression tests.

--- app/models/media.rb
+++ app/models/media.rb
@@ -80,6 +80,9 @@ class Media < ActiveRecord::Base
     return "url" if url.present?
   end
 
+  # TODO: Add regression tests for cases we'd like not to break.
+  # i.e. search 'oracoes coordenadas' and find 'exercícios 8 ano conjunções
+  # coordenativas'
   def self.search(options = {})
     query_string = options[:q].present? ? options[:q] : '*'
     query = {
