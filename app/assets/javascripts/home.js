7806f69c951d378ee0c1a379bd52c5c1373a9a28
Leo Tartari <ltartari@gmail.com>
2015-06-19 16:43:07 -0300

    Update screenshots and details.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,20 @@
 //= require jquery
 //= require foundation/foundation
+//= require foundation/foundation.topbar
 
 $(function () {
   $(document).foundation();
+
+  $('.feature').on('click', function (e) {
+    e.preventDefault();
+    var _self = $(this);
+    var index = _self.data('feature');
+    if (!_self.hasClass('active')) {
+      $('.feature.active').removeClass('active');
+      $(this).toggleClass('active');
+      $('#features__screenshot')
+        .removeClass()
+        .addClass('features__screenshot showing__feature__'+index);
+    }
+  });
 });
