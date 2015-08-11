e389cbff9551742bb4222bf9a4a3e8676a7ae3f0
Pedro Nascimento <pnascimento@gmail.com>
2015-08-11 16:20:13 -0300

    Adding Membership.as_teacher.

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,6 +2,8 @@ class Membership < ActiveRecord::Base
   belongs_to :profile
   belongs_to :course
 
+  scope :as_teacher, -> { where(role: 'teacher') }
+
   default_scope -> { order(:id) }
 
   # TODO: validate presence of course (need to use inverse_of on the association)
