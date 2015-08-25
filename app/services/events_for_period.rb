e39b02657714144d51f84e1b671d401cb545e1ab
Pedro Nascimento <pnascimento@gmail.com>
2015-08-25 18:46:15 -0300

    Refactoring EventsForPeriod.
    We’re now handling one of the null checks on CourseScheduler.

--- app/services/events_for_period.rb
+++ app/services/events_for_period.rb
@@ -3,7 +3,7 @@ class EventsForPeriod
 
   def initialize(course, time_range)
     @course = course
-    set_ranges! time_range_to_date_range(time_range)
+    @past_range, @future_range = split_past_and_future_events(time_range)
   end
 
   def events
@@ -17,21 +17,17 @@ class EventsForPeriod
   end
 
   def future_events
-    if future_range.begin.present?
-      CourseScheduler.new(course, future_range).events
-    else
-      []
+    CourseScheduler.new(course, future_range).events
+  end
+
+  def split_past_and_future_events(time_range)
+    date_range = time_range_to_date_range(time_range)
+
+    past_dates, future_dates = date_range.partition do |date|
+      date <= Date.tomorrow
     end
-  end
 
-  def tomorrow
-    1.day.from_now.to_date
-  end
-
-  def set_ranges!(date_range)
-    past_dates, future_dates = date_range.partition { |date| date <= tomorrow }
-    @past_range = array_to_time_range(past_dates)
-    @future_range = array_to_time_range(future_dates)
+    [array_to_time_range(past_dates), array_to_time_range(future_dates)]
   end
 
   def time_range_to_date_range(time_range)
