e0e737f3d48a3779d68c32e5513811432dcb13a1
mrodrigues <mrodrigues.uff@gmail.com>
2015-08-25 13:43:25 -0300

    Adding some edge cases

--- app/services/events_for_period.rb
+++ app/services/events_for_period.rb
@@ -1,9 +1,9 @@
 class EventsForPeriod
-  attr_reader :course, :time_range
+  attr_reader :course, :past_range, :future_range
 
   def initialize(course, time_range)
     @course = course
-    @time_range = time_range
+    set_ranges! time_range_to_date_range(time_range)
   end
 
   def events
@@ -13,14 +13,33 @@ class EventsForPeriod
   private
 
   def past_events
-    course.events.where(start_at: time_range.begin..today)
+    course.events.where(start_at: past_range)
   end
 
   def future_events
-    CourseScheduler.new(course, today..time_range.end).events
+    if future_range.begin.present?
+      CourseScheduler.new(course, future_range).events
+    else
+      []
+    end
   end
 
-  def today
-    1.day.from_now.end_of_day
+  def tomorrow
+    1.day.from_now.to_date
+  end
+
+  def set_ranges!(date_range)
+    past_dates, future_dates = date_range.partition { |date| date <= tomorrow }
+    @past_range = array_to_time_range(past_dates)
+    @future_range = array_to_time_range(future_dates)
+  end
+
+  def time_range_to_date_range(time_range)
+    time_range.begin.to_date..time_range.end.to_date
+  end
+
+  def array_to_time_range(array)
+    return nil..nil if array.empty?
+    array.first.in_time_zone.beginning_of_day..array.last.in_time_zone.end_of_day
   end
 end
--- spec/services/events_for_period_spec.rb
+++ spec/services/events_for_period_spec.rb
@@ -8,12 +8,12 @@ describe EventsForPeriod do
   let(:fifth_date) { Time.zone.parse('2015-08-31 09:00') }
 
   let(:weekly_schedule) { create(:weekly_schedule, weekday: 1, start_time: '09:00', end_time: '11:00') }
-  let(:course) { create(:course, weekly_schedules: [weekly_schedule], start_date: first_date, end_date: fifth_date) }
+  let(:course) { create(:course, weekly_schedules: [weekly_schedule], start_date: first_date - 2.months, end_date: first_date + 2.months) }
   let(:service) { EventsForPeriod.new(course, WholePeriod.new(today).month) }
 
   before do
     Timecop.travel today
-    PersistPastEvents.new(course).persist!
+    PersistPastEvents.new(course, since: course.start_date).persist!
     weekly_schedule.update!(start_time: '16:00', end_time: '18:00')
     course.reload
   end
@@ -36,5 +36,21 @@ describe EventsForPeriod do
     it { expect(subject.count).to eq 5 }
     it { expect(subject.first(2).all?(&:persisted?)).to be true }
     it { expect(subject.last(3).any?(&:persisted?)).to be false }
+  end
+
+  context "whole range after today" do
+    let(:today) { second_date }
+    let(:service) { EventsForPeriod.new(course, WholePeriod.new(today + 1.month).month) }
+
+    it { expect(subject.count).to eq 4 }
+    it { expect(subject.any?(&:persisted?)).to be false }
+  end
+
+  context "whole range before today" do
+    let(:today) { second_date }
+    let(:service) { EventsForPeriod.new(course, WholePeriod.new(today - 1.month).month) }
+
+    it { expect(subject.count).to eq 4 }
+    it { expect(subject.all?(&:persisted?)).to be true }
   end
 end
