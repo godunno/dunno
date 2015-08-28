f30b3e4dd156a524081ecb05414c9cd5e578d564
mrodrigues <mrodrigues.uff@gmail.com>
2015-08-28 17:55:17 -0300

    Removing invitation

--- app/mailers/registrations_mailer.rb
+++ app/mailers/registrations_mailer.rb
@@ -1,12 +1,6 @@
 class RegistrationsMailer < Devise::Mailer
   include Roadie::Rails::Automatic
 
-  def invitation(user_or_id)
-    @user = User.find(user_or_id)
-    @token = @user.invitation_token
-    mail(to: @user.email, subject: 'Bem-vindo(a) ao Dunno!')
-  end
-
   def successful_registration(user_or_id)
     fail "Disabled, please see #819"
     @user = User.find(user_or_id)
