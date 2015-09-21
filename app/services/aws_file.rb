0ff16dcbb1ff67d168d50895123cf48d061e871d
Pedro Nascimento <pnascimento@gmail.com>
2015-09-21 19:45:34 -0300

    Setting region for our assets.

--- app/services/aws_file.rb
+++ app/services/aws_file.rb
@@ -21,6 +21,11 @@ class AwsFile
   end
 
   def storage
-    @storage ||= Fog::Storage.new(provider: 'AWS', aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"], aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"])
+    @storage ||= Fog::Storage.new(
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: ENV['AWS_REGION']
+    )
   end
 end
