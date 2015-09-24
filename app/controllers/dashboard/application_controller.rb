5efffe00c520386ce6cd16e86afce9069110a798
Pedro Nascimento <pnascimento@gmail.com>
2015-09-24 13:46:55 -0300

    If user is signed in, redirect it to the app on sign up.

--- app/controllers/dashboard/application_controller.rb
+++ app/controllers/dashboard/application_controller.rb
@@ -15,6 +15,10 @@ class Dashboard::ApplicationController < ApplicationController
   end
 
   def sign_up
-    render layout: 'sign_in'
+    if user_signed_in?
+      redirect_to after_sign_in_path_for(current_user)
+    else
+      render layout: 'sign_in'
+    end
   end
 end
