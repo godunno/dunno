c6b02688160cd319851b79edccd7bef29cee7316
Pedro Nascimento <pnascimento@gmail.com>
2015-09-23 23:29:28 -0300

    Add AuthenticateUserFromFacebook service.

---
+++ app/services/authenticate_user_from_facebook.rb
@@ -0,0 +1,36 @@
+class AuthenticateUserFromFacebook
+  def initialize(omniauth_hash)
+    @facebook_uid = omniauth_hash["uid"]
+    @user_info = parse_user_info(omniauth_hash["info"])
+  end
+
+  def authenticate
+    (find_user_by_email || find_or_create_user_by_facebook_uid).tap do |user|
+      user.update(facebook_uid: facebook_uid, avatar_url: user_info[:avatar_url])
+    end
+  end
+
+  private
+
+  attr_reader :facebook_uid, :user_info
+
+  def find_user_by_email
+    User.find_by(email: user_info[:email])
+  end
+
+  def find_or_create_user_by_facebook_uid
+    User.find_or_initialize_by(facebook_uid: facebook_uid) do |u|
+      u.name = user_info[:name]
+      u.email = user_info[:email]
+      u.password = Devise.friendly_token[0, 20]
+    end
+  end
+
+  def parse_user_info(info)
+    {
+      name: info["name"],
+      email: info["email"],
+      avatar_url: info["image"]
+    }
+  end
+end
---
+++ spec/services/authenticate_user_from_facebook_spec.rb
@@ -0,0 +1,71 @@
+require "spec_helper"
+
+describe AuthenticateUserFromFacebook do
+  describe "logging with facebook" do
+    let(:omniauth_hash) do
+      {
+        "provider" => "facebook",
+        "uid" => "12345678",
+        "info" => {
+          "email" => "darthvader@example.org",
+          "name" => "Darth Vader",
+          "image" => "http://graph.facebook.com/awesome_photo.png"
+        },
+        "credentials" => {
+          "token" => "...",
+          "expires_at" => 1_441_880_921,
+          "expires" => true
+        },
+        "extra" => {
+          "raw_info" => {
+            "name" => "xxx",
+            "email" => "xxxxxx@gmail.com",
+            "id" => "xxxxxxxxxx"
+          }
+        }
+      }
+    end
+
+    let(:authenticate_with_facebook) do
+      AuthenticateUserFromFacebook.new(omniauth_hash).authenticate
+    end
+
+    context "as a new user" do
+      let(:created_user) { authenticate_with_facebook }
+
+      it { expect(created_user).to be_a User }
+      it { expect(created_user.facebook_uid).to eq "12345678" }
+      it { expect(created_user.name).to eq "Darth Vader" }
+      it { expect(created_user.email).to eq "darthvader@example.org" }
+      it { expect(created_user.avatar_url).to eq "http://graph.facebook.com/awesome_photo.png" }
+      it { expect(created_user).to be_persisted }
+    end
+
+    context "as an existent user" do
+      context "with no previous facebook info" do
+        let!(:user) { create(:user, email: "darthvader@example.org") }
+
+        before do
+          authenticate_with_facebook
+          user.reload
+        end
+
+        it { expect(user.facebook_uid).to eq "12345678" }
+        it { expect(user.email).to eq "darthvader@example.org" }
+        it { expect(user.avatar_url).to eq "http://graph.facebook.com/awesome_photo.png" }
+      end
+      context "with previous facebook info and a different email address" do
+        let!(:user) { create(:user, email: "previous_email@example.org", facebook_uid: "12345678") }
+
+        before do
+          authenticate_with_facebook
+          user.reload
+        end
+
+        it { expect(user.facebook_uid).to eq "12345678" }
+        it { expect(user.email).to eq "previous_email@example.org" }
+        it { expect(user.avatar_url).to eq "http://graph.facebook.com/awesome_photo.png" }
+      end
+    end
+  end
+end
