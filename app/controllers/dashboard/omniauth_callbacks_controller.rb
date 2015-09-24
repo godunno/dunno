b88f2f90455f4688c6db6aca67773f7dd9b58357
Pedro Nascimento <pnascimento@gmail.com>
2015-09-24 13:50:14 -0300

    Allow user to sign in with facebook.

--- app/controllers/dashboard/omniauth_callbacks_controller.rb
+++ app/controllers/dashboard/omniauth_callbacks_controller.rb
@@ -1,4 +1,7 @@
 class Dashboard::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+    @user = AuthenticateUserFromFacebook.new(request.env["omniauth.auth"]).authenticate
+    sign_in @user, event: :authentication
+    redirect_to request.env["omniauth.origin"] || after_sign_in_path_for(@user)
   end
 end
--- spec/controllers/dashboard/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/dashboard/omniauth_callbacks_controller_spec.rb
@@ -1,5 +1,49 @@
 require "spec_helper"
 
 describe Dashboard::OmniauthCallbacksController do
+  describe "#facebook" do
+    before { stub_env_for_omniauth }
+    def do_action
+      get :facebook
+    end
 
+    describe "authenticating a new user" do
+      it "creates a new user" do
+        expect { do_action }.to change(User, :count).by(1)
+      end
+
+      it "signs the user in" do
+        do_action
+        expect(subject.current_user).to be_present
+      end
+
+      it "redirects to previous location if present" do
+        request.env["omniauth.origin"] = "http://example.org/previous_page"
+        do_action
+        expect(subject).to redirect_to "http://example.org/previous_page"
+      end
+
+      it "redirects to angular app when there is no previous location" do
+        do_action
+        expect(subject).to redirect_to dashboard_path
+      end
+    end
+
+    describe "authenticating an existent user" do
+      let!(:user) { create(:user, email: "darthvader@example.org") }
+      it "does not create a new user" do
+        expect { do_action }.to_not change(User, :count)
+      end
+
+      it "signs the user in" do
+        do_action
+        expect(subject.current_user).to eq user
+      end
+    end
+  end
+
+  def stub_env_for_omniauth
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+  end
 end
