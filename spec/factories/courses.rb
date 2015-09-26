a61a0444378aba4e9609304a9db6414d1c6e60f8
mrodrigues <mrodrigues.uff@gmail.com>
2015-09-25 21:09:16 -0300

    Refactoring request tests

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -7,5 +7,41 @@ FactoryGirl.define do
     class_name "101"
     institution "PUC-Rio"
     teacher { build(:profile) }
+
+    trait :with_events do
+      start_date { Date.today }
+
+      transient do
+        start_time '09:00'
+        end_time '11:00'
+      end
+
+      weekly_schedules do
+        [
+          create(:weekly_schedule,
+                 weekday: start_date.to_date.wday,
+                 start_time: start_time,
+                 end_time: end_time)
+        ]
+      end
+
+      events do
+        base_start_at = start_date
+                        .to_date
+                        .beginning_of_day
+                        .change(hour: start_time)
+        base_end_at = base_start_at.change(hour: end_time)
+
+        {
+          published: 0.weeks,
+          canceled: 1.week,
+          draft: 2.weeks
+        }.map do |status, weeks|
+          build(:event, status,
+                start_at: base_start_at + weeks,
+                end_at: base_end_at + weeks)
+        end
+      end
+    end
   end
 end
