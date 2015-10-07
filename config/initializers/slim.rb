123ca57bd1704d239aacde04cbb5600bb5ae5850
Pedro Nascimento <pnascimento@gmail.com>
2015-10-07 14:23:30 -0300

    Adding helpers to the asset pipeline in the correct place for Sprockets 4.

--- config/initializers/slim.rb
+++ config/initializers/slim.rb
@@ -1,6 +1,1 @@
 ::Slim::Engine.set_options attr_list_delims: { '(' => ')', '[' => ']' }
-
-Rails.application.assets.context_class.class_eval do
-  include ActionView::Helpers
-  include Rails.application.routes.url_helpers
-end
