72be40869b18abb434e75a42baf93279c95d045a
mrodrigues <mrodrigues.uff@gmail.com>
2015-10-22 17:38:28 -0200

    Don't allow students to create events when commenting

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,6 +1,12 @@
 class CommentPolicy < ApplicationPolicy
+  delegate :event, to: :record
+  delegate :course, to: :event
+
   def create?
-    can_see_event?
+    can_see_event? &&
+      (teacher? ||
+       event_exists? ||
+       event_scheduled?)
   end
 
   alias_method :update?, :create?
@@ -9,6 +15,18 @@ class CommentPolicy < ApplicationPolicy
   private
 
   def can_see_event?
-    !!(record.event && EventPolicy.new(profile, record.event).show?)
+    !!(event && EventPolicy.new(profile, event).show?)
+  end
+
+  def event_exists?
+    event.persisted?
+  end
+
+  def teacher?
+    profile.role_in(course) == 'teacher'
+  end
+
+  def event_scheduled?
+    FindWeeklySchedule.new(event.start_at, course.weekly_schedules).weekly_schedule?
   end
 end
